79eabdb5bee4804fb71dd15869caf779ea1528c7
Mostafa3elewa <mostafa.ahmed.elewa@gmail.com>
2023-04-04 00:59:23 +0200

    - fix tournmenet table

--- src/components/touranmentsTable.js
+++ src/components/touranmentsTable.js
@@ -1,57 +1,45 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Button } from "react-bootstrap";
 
-function createData(
-  name,
-  calories,
-  fat,
-  carbs,
-  protein
-) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-export default function BasicTable() {
+export default function BasicTable({ objects, tournments }) {
+  const keys = Object.keys(objects);
+  console.log(keys);
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Touranment Id</TableCell>
-            <TableCell align="right">Category</TableCell>
-            <TableCell align="right">Start Time</TableCell>
-            <TableCell align="right">Duration</TableCell>
-            <TableCell align="right">Show More</TableCell>
-            <TableCell align="right">Delete</TableCell>
+            {keys.map((key) => {
+              return (
+                <TableCell key="head" align="right">
+                  {objects[key]}
+                </TableCell>
+              );
+            })}
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
+          {tournments.map((tournment) => (
+            <TableRow key={tournment.id} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+              {keys.map((key) => {
+                return (
+                  <TableCell key={tournment.id} align="right">
+                    {tournment[key]}
+                  </TableCell>
+                );
+              })}
+              <TableCell>
+                <Button size="sm" variant="info" className="button">
+                  Show More
+                </Button>
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
             </TableRow>
           ))}
         </TableBody>
--- src/pages/touranments.js
+++ src/pages/touranments.js
@@ -1,32 +1,70 @@
-import Head from 'next/head';
-import { Box, Container } from '@mui/material';
-import { Layout as DashboardLayout } from "src/layouts/dashboard/layout";
-import BasicTable from 'src/components/touranmentsTable';
+import Head from "next/head";
+import { Box, Container } from "@mui/material";
+import axios from "axios";
 
-function Page() {
-    return (
-        <>
-    <Head>
-      <title>
-        Touranments | Devias Kit
-      </title>
-    </Head>
-    <Box
-      component="main"
-      sx={{
-        flexGrow: 1,
-        py: 8
-      }}
-    >
-      <Container maxWidth="lg">
-        <BasicTable />
-      </Container>
-    </Box>
-  </>
-    );
-  }
-  
-  Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
-  
-  export default Page;
-  
+import { Layout as DashboardLayout } from "src/layouts/dashboard/layout";
+import BasicTable from "src/components/touranmentsTable";
+import { useState, useEffect } from "react";
+
+const baseURL = "http://localhost:5000/api/v1/tournament/all";
+
+const keys = {
+  title: "title",
+  category: "category",
+  sort_order: "sort order",
+  max_size: "maximum size",
+  start_time: "start time",
+  operator: "operator",
+  "": "",
+  "": "",
+};
+function Page({}) {
+  const token = localStorage.getItem("jwt");
+  console.log(token);
+  const [tournment, setTournment] = useState();
+  // const tournment = [1, 2];
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const token = localStorage.getItem("jwt");
+      console.log(token);
+
+      const response = await axios.get(baseURL, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = response;
+      console.log("🚀 ~ file: touranments.js:48 ~ fetchData ~ data:", data.data);
+      setTournment(data.data);
+      setLoading(false);
+
+      console.log(typeof data.data);
+    };
+    fetchData();
+  }, []);
+  console.log(tournment);
+  return (
+    <>
+      <Head>
+        <title>Touranments | Devias Kit</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+        <Container maxWidth="lg">
+          {loading ? <h1>loading</h1> : <BasicTable objects={keys} tournments={tournment} />}
+        </Container>
+      </Box>
+    </>
+  );
+}
+
+Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
+
+export default Page;
