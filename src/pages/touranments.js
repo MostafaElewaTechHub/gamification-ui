15cc7142500a3548a9a8540889bdb5dda65ca1e9
mohamedehab00 <mohamed.ehab31450@gmail.com>
2023-04-04 00:56:37 +0200

    Mohamed Ehab committed

--- src/pages/touranments.js
+++ src/pages/touranments.js
@@ -1,32 +1,29 @@
-import Head from 'next/head';
-import { Box, Container } from '@mui/material';
+import Head from "next/head";
+import { Box, Container } from "@mui/material";
 import { Layout as DashboardLayout } from "src/layouts/dashboard/layout";
-import BasicTable from 'src/components/touranmentsTable';
+import BasicTable from "src/components/touranmentsTable";
 
 function Page() {
-    return (
-        <>
-    <Head>
-      <title>
-        Touranments | Devias Kit
-      </title>
-    </Head>
-    <Box
-      component="main"
-      sx={{
-        flexGrow: 1,
-        py: 8
-      }}
-    >
-      <Container maxWidth="lg">
-        <BasicTable />
-      </Container>
-    </Box>
-  </>
-    );
-  }
-  
-  Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
-  
-  export default Page;
-  
+  return (
+    <>
+      <Head>
+        <title>Touranments | Devias Kit</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+        <Container maxWidth="lg">
+          <BasicTable />
+        </Container>
+      </Box>
+    </>
+  );
+}
+
+Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
+
+export default Page;
